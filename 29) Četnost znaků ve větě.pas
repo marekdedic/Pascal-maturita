5758170722835b3dae4af60c177fa2c331c5f906
Marek Dědič <developer@dedic.eu>
2014-04-15 20:56:41 +0200

    completely redone 29

--- 29) Četnost znaků ve větě.pas
+++ 29) Četnost znaků ve větě.pas
@@ -1,69 +1,20 @@
-type pnext = ^chars;
-chars = record
-  ch: char;
-  count: integer;
-  next: pnext;
-end;
-
-var str: string;
-first: pnext;
-
-procedure append(ch:char);
-var c,n,l:pnext;
-found:boolean;
-begin
-  c:=first;
-  found:=false;
-  while (c<>nil) AND (found=false) do
-  begin
-    if (c^.ch=ch) then
-    begin
-      found:=true;
-      c^.count:=c^.count+1;
-    end;
-    l:=c;
-    c:=c^.next;
-  end;
-  if (found=false) then
-  begin
-    if (first=nil) then
-    begin
-      new(first);
-      first^.ch:=ch;
-      first^.count:=1;
-    end
-    else
-    begin
-      new(n);
-      n^.ch:=ch;
-      n^.count:=1;
-      l^.next:=n;
-    end;
-  end;
-end;
-
-procedure foreach(s:string);
-var i:integer;
-begin
-for i:=1 to length(s) do
-  append(s[i]);
-end;
-
-procedure print_count();
-var c:pnext;
-begin
-  c:=first;
-  while (c<>nil) do
-  begin
-    writeln('Pismenko ',c^.ch,' - ',c^.count,'x');
-    c:=c^.next;
-  end;
-end;
+var chars : array['A'..'Z'] of integer;
+abeceda : set of char;
+ch : char;
+i : integer;
+veta : string;
 
 begin
-writeln('Napiste vetu');
-readln(str);
-foreach(str);
-print_count();
+     abeceda:=['A'..'Z'];
+     Writeln('Napis vetu:');
+     Readln(veta);
+for i:= 0 to length(veta) do
+begin
+  if (upcase(veta[i]) in abeceda) then
+    chars[upcase(veta[i])]:=chars[upcase(veta[i])]+1;
+end;
+for ch:='A' to 'Z' do
+  if (chars[ch]>0) then
+    writeln(ch,' - ',chars[ch],'x');
 readln;
 end.
